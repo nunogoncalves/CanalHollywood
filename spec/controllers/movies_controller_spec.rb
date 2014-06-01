c0be8e5719030bd8b0fe5f05c39910b9e7d3bcd9
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2014-06-01 08:24:03 +0100

    test folder

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe MoviesController do
+  describe 'PUT#SYNC' do
+    it 'should create movies' do
+      pending
+    end
+  end
+end
---
+++ spec/support/requests/json_helpers.rb
@@ -0,0 +1,9 @@
+module Requests
+  module JsonHelpers
+
+    def json
+      @json ||= JSON.parse(response.body)
+    end
+
+  end
+end
